51c2c124adab1efa9ae205032825a335936dc1a4
Bradley Davis <bad884@me.com>
2018-11-02 21:44:42 +0000

    Update init

--- systems/ceph/init.py
+++ systems/ceph/init.py
@@ -5,34 +5,32 @@ import rados
 import sys
 
 
-# try:
-#         # cluster = rados.Rados(conffile = '/etc/ceph/ceph.conf', conf = dict (keyring = '/etc/ceph/ceph.client.admin.keyring'))
-#         cluster = rados.Rados(conf = dict (keyring = '/etc/ceph/ceph.client.admin.keyring'))
-# 	print "Created cluster handle."
-# except TypeError as e:
-#         print 'Argument validation error: ', e
-#         raise e
+try:
+        # cluster = rados.Rados(conffile = '/etc/ceph/ceph.conf', conf = dict (keyring = '/etc/ceph/ceph.client.admin.keyring'))
+        cluster = rados.Rados(conffile = '/home/ceph-admin/CORDS/systems/ceph/confs/bradley/ceph.conf', conf = dict (keyring = '/etc/ceph/ceph.client.admin.keyring'))
+	print "Created cluster handle."
+except TypeError as e:
+        print 'Argument validation error: ', e
+        raise e
 
-# try:
-#         cluster.connect()
-# 	print "Connected to the cluster."
-# except Exception as e:
-#         print "Connection error: ", e
-#         raise e
-
-
-##################################################
-# cluster = rados.Rados()
-cluster = rados.Rados(conf = dict (keyring = '/etc/ceph/ceph.client.admin.keyring'))
-print "\nlibrados version: " + str(cluster.version())
 print "Will attempt to connect to: " + str(cluster.conf_get('mon initial members'))
+try:
+        cluster.connect()
+	print "Connected to the cluster."
+except Exception as e:
+        print "Connection error: ", e
+        raise e
 
-cluster.connect()
 print "\nCluster ID: " + cluster.get_fsid()
+print "Librados Version: " + str(cluster.version())
 
-print "\n\nCluster Statistics"
+print "\nCluster Statistics"
 print "=================="
 cluster_stats = cluster.get_cluster_stats()
 
 for key, value in cluster_stats.iteritems():
 	print key, value
+
+print "\nPools"
+print "=================="
+print cluster.list_pools()
